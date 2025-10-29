dd813bbb21125016888d6bb14e0668e32c24c0f8
dudu <2497510095@qq.com>
2025-10-29 17:23:14 +0800

    deplou.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-# Docker 镜像名
+# ------------------------
+# 配置
+# ------------------------
 IMAGE_NAME=jenkins-test-app
+CONTAINER_NAME=jenkins-test-app-run
+HOST_PORT=8081
+CONTAINER_PORT=10001
 
+# ------------------------
 # 构建 Docker 镜像
+# ------------------------
+echo "构建 Docker 镜像: $IMAGE_NAME"
 docker build -t $IMAGE_NAME $WORKSPACE
 
+# ------------------------
 # 停止并删除旧容器（如果存在）
-if [ $(docker ps -aq -f name=jenkins-test) ]; then
-    docker rm -f jenkins-test
+# ------------------------
+EXISTING_CONTAINER=$(docker ps -aq -f name=$CONTAINER_NAME)
+if [ "$EXISTING_CONTAINER" ]; then
+    echo "删除旧容器: $CONTAINER_NAME"
+    docker rm -f $CONTAINER_NAME
 fi
 
+# ------------------------
 # 启动新容器
-docker run -d -p 8888:8888 --name jenkins-test $IMAGE_NAME
+# ------------------------
+echo "启动新容器: $CONTAINER_NAME"
+docker run -d -p $HOST_PORT:$CONTAINER_PORT --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "部署完成！访问 http://localhost:$HOST_PORT"
